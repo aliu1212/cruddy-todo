0efadfe4402ac1f80155b9bdd62aee144af15024
aliu1212 <alexliu12@gmail.com>
2018-12-03 18:57:08 -0800

    Built all functionality, specs passing 100%

--- datastore/index.js
+++ datastore/index.js
@@ -92,14 +92,31 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+
+  //new version-
+  let todoPath = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(todoPath, (error, id) => {
+    if (error) {
+      callback(error);
+    } else {
+      fs.unlink(todoPath, (error, id) => {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, id);
+        }
+      })
+    }
+  });
+
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
